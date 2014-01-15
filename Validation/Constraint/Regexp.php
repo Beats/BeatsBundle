7ba1db3595286d813b7a8fe059c74a817c557169
Aleksandar Ostojić <ion@gigz.com>
2014-01-15 15:45:02 +0100

    Regexp Constraint modified to persist matches

--- Validation/Constraint/Regexp.php
+++ Validation/Constraint/Regexp.php
@@ -1,14 +1,14 @@
 <?php
 namespace BeatsBundle\Validation\Constraint;
 
+use BeatsBundle\Exception\Exception;
 use BeatsBundle\Validation\Constraint;
 use BeatsBundle\Validation\Context;
-use BeatsBundle\Exception\Exception;
 
 class Regexp extends Constraint {
 
   protected $_pattern;
-
+  protected $_matches;
 
   public function __construct($pattern = '#.*#', $failure = 'The value is invalid', $success = 'The value is valid') {
     parent::__construct($failure, $success);
@@ -19,7 +19,7 @@ class Regexp extends Constraint {
   }
 
   public function validate($value, Context $context) {
-    return preg_match($this->_pattern, $value);
+    return preg_match($this->_pattern, $value, $this->_matches);
   }
 
   public function transform($value) {
