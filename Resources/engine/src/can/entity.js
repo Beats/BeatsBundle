d33b86c8680c6f4fd062ccb389ce5fc468416d4c
Aleksandar Ostojić <ion@gigz.com>
2014-01-22 16:17:47 +0100

    Beats.Entity refactored

--- Resources/engine/src/can/entity.js
+++ Resources/engine/src/can/entity.js
@@ -8,18 +8,35 @@
     const: {
     }
   }, {
+    /**
+     * Raw data
+     * @var object
+     */
+    $: null,
+    
+    /**
+     * Lazy loaded
+     * @var object
+     */
     _: null,
-    data: null,
 
     init: function (data) {
       var self = this
       self._super.apply(self, arguments)
-      self.data = data
+      self.$ = data
       self._ = {}
+
+      $.each(data, function (key, val) {
+        if (self[key] === undefined) {
+          self[key] = self.proxy(function () {
+            return this.$[key]
+          })
+        }
+      })
     },
 
     getID: function () {
-      return this.data[this.construct.id]
+      return this.$[this.constructor.id]
     }
 
   })
