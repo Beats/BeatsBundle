e456be94fd4f96b5016bbe55cf3402492b7d8e3e
Aleksandar Ostojić <aleksandar.ostojic@rt-rk.com>
2015-04-21 12:36:05 +0200

    Updated dependency versions

--- Resources/public/js/jquery-extend.js
+++ Resources/public/js/jquery-extend.js
@@ -6,36 +6,36 @@
 
   $.fn.extend({
     serializeObject: function () {
-      var object = {}
+      var object = {};
       $.each(this.serializeArray(), function (idx, item) {
         if (object[item.name] === undefined) {
           object[item.name] = item.value;
         } else {
-          var array = object[item.name]
+          var array = object[item.name];
           if (!array.push) {
             array = [array];
           }
           array.push(item.value);
         }
-      })
+      });
       return object
     }
-  })
+  });
 
   $.extend($, {
     count: function (value) {
       if (value) {
         switch ($.type(value)) {
           case 'array':
-            return value.length
+            return value.length;
           case 'object':
             if (Object && $.isFunction(Object.keys)) {
               return Object.keys(value).length
             }
-            var count = 0
+            var count = 0;
             $.each(value, function () {
               count++
-            })
+            });
             return count
         }
         return 1
@@ -47,7 +47,7 @@
       if (value) {
         switch ($.type(value)) {
           case 'array':
-            return value.join(separator)
+            return value.join(separator);
           case 'object':
             return $.map(value,function (value) {
               return value
@@ -57,6 +57,6 @@
       }
       return ''
     }
-  })
+  });
 
 })(jQuery);
