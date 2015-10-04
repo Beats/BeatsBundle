6604d7eefa064ddba4623f8041bac87724c8eb89
Aleksandar Ostojić <o100ja@gmail.com>
2015-10-04 21:23:46 +0200

    AuthInterface extended

--- Security/User/AuthInterface.php
+++ Security/User/AuthInterface.php
@@ -3,8 +3,14 @@ namespace BeatsBundle\Security\User;
 
 interface AuthInterface {
 
+  const KIND_DIRECT = 'direct';
+
   public function getUserID();
 
   public function generateCode();
+
+  public function isKind($kind = self::KIND_DIRECT);
+
+  //public function getKind();
 
 }
